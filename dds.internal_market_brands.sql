dee1ca86d8d24ece7f08803eebcff6322b7a7b20
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2024-02-01 12:17:00 +0300

    v0

--- dds.internal_market_brands.sql
+++ dds.internal_market_brands.sql
@@ -16,4 +16,5 @@ FROM sq
 LEFT JOIN dds.internal_market_brands b
     ON COALESCE(sq.brand, '0') = COALESCE(b.brand, '0')
     AND COALESCE(sq.country_brand, '0') = COALESCE(b.country, '0')
-WHERE b.brand IS NULL;
+WHERE b.brand IS NULL
+RETURNING COUNT(*);
