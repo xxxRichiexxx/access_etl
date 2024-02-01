6fe517ee86548818f3dd61959cd8bf83dd770bfa
Швейников Андрей Борисович <ShveynikovAB@st.tech>
2024-02-01 12:15:38 +0300

    v0

--- dds.internal_market_brands.sql
+++ dds.internal_market_brands.sql
@@ -9,7 +9,9 @@ sq AS(
     WHERE date_oper BETWEEN '{{ti.xcom_pull(key='min_date', task_ids='Загрузка_данных_в_stage_слой.get_data')}}'
         AND '{{ti.xcom_pull(key='max_date', task_ids='Загрузка_данных_в_stage_слой.get_data')}}'
 )
-SELECT *
+SELECT
+    sq.brand
+    ,sq.country_brand
 FROM sq
 LEFT JOIN dds.internal_market_brands b
     ON COALESCE(sq.brand, '0') = COALESCE(b.brand, '0')
